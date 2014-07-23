33283f6bccd9f9d01301332990b080c77fc50e28
Thomas Dziedzic <gostrc@gmail.com>
2014-07-23 14:56:15 +0000

    rootfs - make password and hostname command line arguments

--- rootfs/configure-rootfs.sh
+++ rootfs/configure-rootfs.sh
@@ -3,8 +3,22 @@
 # exit immediately when a command returns a nonzero
 set -e
 
-PASSWORD='pocketanvl'
-HOSTNAME='anvl'
+while getopts ':p:H:' flag; do
+  case ${flag} in
+    p)
+      config_password="${OPTARG}"
+      ;;
+    H)
+      config_hostname="${OPTARG}"
+      ;;
+    :)
+      die '%s: option requires an argument: '\''%s'\' "${0##*/}" "${OPTARG}"
+      ;;
+    ?)
+      die '%s: invalid option: '\''%s'\' "${0##*/}" "${OPTARG}"
+      ;;
+  esac
+done
 
 # enable logging in through serial
 systemctl enable getty@ttyGS0
@@ -23,8 +37,8 @@ systemctl enable nmbd
 systemctl enable smbd.socket
 
 # change root password
-echo "root:${PASSWORD}" | chpasswd
+echo "root:${config_password}" | chpasswd
 
 # change the hostname
-echo ${HOSTNAME} > /etc/hostname
-sed -i "s/^127.0.0.1.*/& ${HOSTNAME}/" /etc/hosts
+echo ${config_hostname} > /etc/hostname
+sed -i "s/^127.0.0.1.*/& ${config_hostname}/" /etc/hosts
--- rootfs/create-rootfs.sh
+++ rootfs/create-rootfs.sh
@@ -3,6 +3,49 @@
 # exit immediately when a command returns a nonzero
 set -e
 
+die() {
+  printf "${@}"
+  echo
+  exit 1
+}
+
+usage() {
+  cat <<EOF
+usage: ${0##*/} [option]
+
+  Options:
+    -p password    Set the password.
+                   Default is pocketanvl.
+    -H hostname    Set the hostname.
+                   Default is anvl.
+    -h             Display this message.
+EOF
+}
+
+config_password='pocketanvl'
+config_hostname='anvl'
+
+while getopts ':p:H:h' flag; do
+  case ${flag} in
+    p)
+      config_password="${OPTARG}"
+      ;;
+    H)
+      config_hostname="${OPTARG}"
+      ;;
+    h)
+      usage
+      exit 0
+      ;;
+    :)
+      die '%s: option requires an argument: '\''%s'\' "${0##*/}" "${OPTARG}"
+      ;;
+    ?)
+      die '%s: invalid option: '\''%s'\' "${0##*/}" "${OPTARG}"
+      ;;
+  esac
+done
+
 if [[ ${UID} -ne 0 ]]; then
     echo "${0} must be run as root"
     exit 1
@@ -20,7 +63,7 @@ cp pacman.conf rootfs/etc/pacman.conf
 
 cp configure-rootfs.sh rootfs
 
-arch-chroot rootfs /configure-rootfs.sh
+arch-chroot rootfs /configure-rootfs.sh -p ${config_password} -H ${config_hostname}
 
 rm rootfs/configure-rootfs.sh
 
