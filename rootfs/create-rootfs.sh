afdcea8e1451b1acd4230fd73a99e919704f83b8
Thomas Dziedzic <gostrc@gmail.com>
2014-07-27 14:31:16 +0000

    add alsa-utils to the packages to install

--- rootfs/create-rootfs.sh
+++ rootfs/create-rootfs.sh
@@ -59,7 +59,7 @@ mkdir "${ROOTFS}"
 pacstrap -C "${DIR}/pacman.conf" -d "${ROOTFS}" base wpa_supplicant openssh \
   sqlite samba graphicsmagick xdelta3 xapian-core chrony base-devel \
   traceroute dialog \
-  omap-idle nodejs nodejs-inotify pacmatic
+  omap-idle nodejs nodejs-inotify pacmatic alsa-utils
 
 # override standard pacman with pacman.conf modified for our repository
 cp "${DIR}/pacman.conf" "${ROOTFS}/etc/"
